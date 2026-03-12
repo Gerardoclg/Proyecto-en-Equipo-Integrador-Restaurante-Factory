5c2b4d832a23e2f6cd6d07088b4ef1bdfc993cf7
LeonardoJDRR <L22210301@tectijuana.edu.mx>
2026-03-11 19:26:29 -0700

    Add files via upload

---
+++ FabricaRestaurante/proyecto/RestauranteChino.cs
@@ -0,0 +1,24 @@
+﻿namespace FabricasRestaurantes
+{
+    public class RestauranteChino : RestauranteFactory
+    {
+        public override PlatoFuerte CrearPlatoFuerte() => new ChowMein();
+        public override Bebida CrearBebida() => new TeJazmin();
+        public override Postre CrearPostre() => new RollitoDulce();
+    }
+
+    class ChowMein : PlatoFuerte
+    {
+        public override string Servir() => "Chow Mein";
+    }
+
+    class TeJazmin : Bebida
+    {
+        public override string Servir() => "Té Jazmín";
+    }
+
+    class RollitoDulce : Postre
+    {
+        public override string Servir() => "Rollito dulce con nieve";
+    }
+}
